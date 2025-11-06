efd97af4bd70bd7a72378d23e1b92c79b8f6093d
yk18b <ykonan@iut.univ-paris8.fr>
2025-11-06 11:58:12 +0100

    Sid utilise l'objet directement+création de la classe parente de Dague et Arc + design pattern Decorateur Dans dague+ cette fos j'i refactor les deux nouveau design pattern pour qu'on puisse les utiliser peux importe l'arme qu'on a

--- src/main/java/modeles/objets/outils/Arme/decorateurArme.java
+++ src/main/java/modeles/objets/outils/Arme/decorateurArme.java
@@ -4,10 +4,10 @@ import modeles.acteur.Sid;
 import modeles.monde.Terrain;
 import modeles.objets.Inventaire;
 
-public class multiArme extends Arme{
+public class decorateurArme extends Arme{
     private Arme arme;
     private String competence;
-   public multiArme(Terrain terrain, Inventaire inventaire, Sid sid, int degat, Arme armee){
+   public decorateurArme(Terrain terrain, Inventaire inventaire, Sid sid, int degat, Arme armee){
        super(terrain,inventaire,sid,degat,armee.getDISTANCE_MAX());
        this.arme = armee;
        this.competence  = this.arme.utiliserCompetence() + utiliserCompetence();
